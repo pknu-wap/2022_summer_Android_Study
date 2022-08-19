ce69464c730bb14d05aae4075f4e9a06ebce673c
jaino <msb3302@naver.com>
2022-08-19 18:32:15 +0900

    Add DongBangApp file

---
+++ app/src/main/java/com/example/dongbangjupsho/DongBangApp.kt
@@ -0,0 +1,14 @@
+package com.example.dongbangjupsho
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DongBangApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, com.example.dongbangjupsho.BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
