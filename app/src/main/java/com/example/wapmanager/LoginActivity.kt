1dbf95bb017010c287deb3ee1730e4bb221f1776
vrexpert <vrexpert2302@gmail.com>
2022-08-29 14:01:00 +0900

    signup complete

--- app/src/main/java/com/example/wapmanager/LoginActivity.kt
+++ app/src/main/java/com/example/wapmanager/LoginActivity.kt
@@ -1,20 +1,27 @@
 package com.example.wapmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wapmanager.databinding.ActivityLoginBinding
 import com.example.wapmanager.databinding.ActivityMainBinding
 import com.example.wapmanager.loginFragments.SigninFragment
 import com.example.wapmanager.loginFragments.SignupFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.ktx.auth
 
 class LoginActivity : AppCompatActivity() {
-    //var signupFragment: SignupFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as SignupFragment
-    //var signinFragment: SigninFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as SigninFragment
+    private val binding by lazy{ActivityLoginBinding.inflate(layoutInflater)}
+    private var auth:FirebaseAuth = Firebase.auth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
         //setFragment()
     }
 
@@ -26,6 +33,7 @@ class LoginActivity : AppCompatActivity() {
     //}
 
     fun goSignup(){
+        val signupFragment = SignupFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.fragmentContainerView,signupFragment)
         transaction.addToBackStack("signup")
@@ -36,5 +44,13 @@ class LoginActivity : AppCompatActivity() {
         onBackPressed()
     }
 
+//    //현재 인증된 사용자라면 main 실행
+//    override fun onStart(){
+//        super.onStart()
+//        if(auth.currentUser != null){
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }
+//    }
 
 }
--- app/src/main/java/com/example/wapmanager/loginFragments/SignupFragment.kt
+++ app/src/main/java/com/example/wapmanager/loginFragments/SignupFragment.kt
@@ -1,19 +1,72 @@
 package com.example.wapmanager.loginFragments
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.wapmanager.LoginActivity
 import com.example.wapmanager.R
+import com.example.wapmanager.databinding.ActivityLoginBinding
+import com.example.wapmanager.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class SignupFragment : Fragment() {
+    private val binding by lazy{ FragmentSignupBinding.inflate(layoutInflater)}
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        auth = Firebase.auth
+        binding.btnSignup.setOnClickListener{
+            registerUser()
+        }
+        return binding.root
+    }
+
+    private fun registerUser(){
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnSuccessListener {
+                            Toast.makeText(
+                                activity,
+                                "회원가입이 성공하였습니다.", Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    withContext(Dispatchers.Main) {
+
+                    }
+
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            activity,
+                            "회원가입이 실패하였습니다.", Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
+    private fun checkLoggedInstate(){
+
     }
 }
