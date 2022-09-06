b5f8d871d546bbfe5ade8a77a9cc802ec20f8ac7
jaino <msb3302@naver.com>
2022-09-06 17:54:46 +0900

    Refactor : (get EnterUser data) suspendCancellableCoroutine -> callbackFlow

--- app/src/main/java/com/example/dongbangjupsho/data/repository/FirebaseDatabaseRepositoryImpl.kt
+++ app/src/main/java/com/example/dongbangjupsho/data/repository/FirebaseDatabaseRepositoryImpl.kt
@@ -8,31 +8,37 @@ import com.example.dongbangjupsho.domain.util.FirebaseManager
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.coroutines.resume
 
+@ExperimentalCoroutinesApi
 class FirebaseDatabaseRepositoryImpl : FirebaseDatabaseRepository {
 
-    override suspend fun getTodayEnterPeople(timeStamp: String): String? =
-        suspendCancellableCoroutine { cont ->
-            FirebaseManager.firebaseDatabase.reference.child(HOME).child("TodayEnter").child(timeStamp)
+    override suspend fun getTodayEnterPeople(timeStamp: String): Flow<String?> =
+        callbackFlow {
+            FirebaseManager.firebaseDatabase.reference.child(HOME).child("TodayEnter")
+                .child(timeStamp)
                 .addChildEventListener(object : ChildEventListener {
                     override fun onChildAdded(
                         snapshot: DataSnapshot,
                         previousChildName: String?
                     ) {
-                        cont.resume(snapshot.childrenCount.toString())
+                        trySend(snapshot.childrenCount.toString())
                     }
-                    override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?, ) {}
+                    override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
                     override fun onChildRemoved(snapshot: DataSnapshot) {}
                     override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
                     override fun onCancelled(error: DatabaseError) {
-                        cont.resume(null)
+                        trySend(null)
                     }
                 })
-    }
+        }
+
 
     override suspend fun setTodayEnterPeople(userEnterInfo: UserEnterInfo): Boolean =
         suspendCancellableCoroutine { cont ->
--- app/src/main/java/com/example/dongbangjupsho/domain/repository/FirebaseDatabaseRepository.kt
+++ app/src/main/java/com/example/dongbangjupsho/domain/repository/FirebaseDatabaseRepository.kt
@@ -1,8 +1,9 @@
 package com.example.dongbangjupsho.domain.repository
 
 import com.example.dongbangjupsho.domain.model.UserEnterInfo
+import kotlinx.coroutines.flow.Flow
 
 interface FirebaseDatabaseRepository {
-    suspend fun getTodayEnterPeople(timeStamp: String): String?
+    suspend fun getTodayEnterPeople(timeStamp: String): Flow<String?>
     suspend fun setTodayEnterPeople(userEnterInfo: UserEnterInfo): Boolean
 }
