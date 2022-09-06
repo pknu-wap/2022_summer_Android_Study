6a62bd011e9f268b92500de561ff220e49dd7c92
jaino <msb3302@naver.com>
2022-09-06 15:31:04 +0900

    Chore : SignInEvent 수정(힌트 Event 제거)

--- app/src/main/java/com/example/dongbangjupsho/presentation/user/signin/UserSignInEvent.kt
+++ app/src/main/java/com/example/dongbangjupsho/presentation/user/signin/UserSignInEvent.kt
@@ -1,13 +1,9 @@
 package com.example.dongbangjupsho.presentation.user.signin
 
-import androidx.compose.ui.focus.FocusState
-
 sealed class UserSignInEvent {
 
-    data class EnteredUserId(val value: String) : UserSignInEvent()
-    data class ChangeUserIdFocus(val focusState: FocusState): UserSignInEvent()
-    data class EnteredPassword(val value: String) : UserSignInEvent()
-    data class ChangePasswordFocus(val focusState: FocusState): UserSignInEvent()
+    data class EmailChanged(val email: String) : UserSignInEvent()
+    data class PasswordChanged(val password: String) : UserSignInEvent()
 
-    object SignInUser : UserSignInEvent()
+    object Submit : UserSignInEvent()
 }
