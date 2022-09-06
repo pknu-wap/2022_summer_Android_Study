21e80d8df8a2eb33237690ac5365daadd13e5501
jaino <msb3302@naver.com>
2022-09-06 15:25:12 +0900

    Chore : remove TextState, TextField file

--- app/src/main/java/com/example/dongbangjupsho/presentation/user/TextFieldState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.dongbangjupsho.presentation.user
-
-data class TextFieldState (
-    val text : String =  "",
-    val hint : String = "",
-    val isHintVisible : Boolean = true
-    )
